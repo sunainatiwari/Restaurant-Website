ee335b92cea8a87513d544fad5e18eb766731e00
Sunaina Tiwari <136787771+sunainatiwari@users.noreply.github.com>
2023-08-28 23:54:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var typed = new Typed('#heading-main', {
-    strings: ['Web Developer', 'Software Engineer', 'Web design'],
+import Typed from 'typed.js';
+
+import Typed from 'typed.js';
+
+const typed = new Typed('#element', {
+    strings: ['<i>Order</i> Hot Meals.', '&amp; Served Superfast.'],
     typeSpeed: 50,
 });
+
+// var typed = new Typed('#element', {
+//     strings: ['Web Developer', 'Software Engineer', 'Web design'],
+//     typeSpeed: 50,
+// });
